764563ff5b4cf0d24bd6ca7045d1706501509e98
Merle Zhang <mikezh2011@163.com>
2021-10-25 14:59:56 +0100

    Add the downloader

---
+++ download.py
@@ -0,0 +1,26 @@
+import os
+
+repo = "https://github.com/drslock/CG2021/archive/refs/heads/main.zip"
+os.system(f"curl -LJO {repo}")
+
+zip = "CG2021-main.zip"
+os.system(f"unzip {zip}")
+
+videos = []
+
+for root, dirs, files in os.walk("."):
+    for file in files:
+        if file.endswith(".mp4"):
+            videos.append(f"{root}/{file}")
+
+os.system("mkdir videos")
+for video in videos:
+    tokens = video.split("/")
+    week = f"week{tokens[3].split()[0]}"
+    task = f"task{tokens[4].split()[0]}"
+    category = tokens[5]
+    index = tokens[6][tokens[6].index("-")+1:tokens[6].index(".")]
+    filename = f"{week}-{task}-{category}-{index}"
+    os.system(f'mv "{video}" ./videos/{filename}.mp4')
+
+os.system("rm -r CG2021-main CG2021-main.zip")
