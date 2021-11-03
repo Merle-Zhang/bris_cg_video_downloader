73665ad6908948597dc01b464647812075be3bd7
Merle Zhang <merle.xzhang@gmail.com>
2021-11-03 15:51:18 +0000

    Add comments

--- download.py
+++ download.py
@@ -1,20 +1,32 @@
+# Download all the videos from the CG workbooks.
+# 
+# Requirement: internet connection, and curl or wget
+# Output: `videos` folder containing all the videos
+
 import os
 
+# Link to the CG repo
 repo = "https://github.com/drslock/CG2021/archive/refs/heads/main.zip"
-# os.system(f"curl -LJO {repo}")
 
+# TODO: Add auto detection for choosing curl or wget,
+#       and exit if both of them are not available
+
+# Download with curl or wget
+# os.system(f"curl -LJO {repo}")
 os.system(f"wget --no-check-certificate --content-disposition {repo}")
 
+# Unzip the file
 zip = "CG2021-main.zip"
 os.system(f"unzip {zip}")
 
+# Collect the path for all the videos
 videos = []
-
 for root, dirs, files in os.walk("."):
     for file in files:
         if file.endswith(".mp4"):
             videos.append(f"{root}/{file}")
 
+# Move the videos into a separate folder
 os.system("mkdir videos")
 for video in videos:
     tokens = video.split("/")
@@ -25,4 +37,5 @@ for video in videos:
     filename = f"{week}-{task}-{category}-{index}"
     os.system(f'mv "{video}" ./videos/{filename}.mp4')
 
+# Remove the zip and unzipped folder
 os.system("rm -r CG2021-main CG2021-main.zip")
--- extract_links.py
+++ extract_links.py
@@ -1,28 +1,39 @@
-import re
-import argparse
+# Extract all the links from the HTML file and update the readme.
+# 
+# Requirement: the HTML file saved from `https://web.microsoftstream.com/studio/videos`
+# Output: The updated readme file
 
+import argparse
+import re
+
+# Pass in the filename for the HTML file as a command line argument
 parser = argparse.ArgumentParser(description='extract links from HTML file')
 parser.add_argument('filename', help='the filename of the html file')
 args = parser.parse_args()
 
+# Read in the HTML
 with open(args.filename, "r") as file:
     txt = file.read()
 
+# Regular expression for file name and stream link
 name = r'week[0-9]{2}\-task[0-9]{2}\-([^\s]+)\-[0-9]'
 link = r'https://web.microsoftstream.com/video/[^"]+'
 
+# Find all the links and then remove the duplicates
 expression = f'aria-label="{name}[^"]+" href="{link}"'
 p = re.compile(expression)
-namelinks = [x.group() for x in re.finditer(expression, txt)]
+namelinks = set([x.group() for x in re.finditer(expression, txt)])
 
+# Build the map {filename -> streamlink}
 links = {re.search(name, namelink).group(): re.search(link, namelink).group() for namelink in namelinks}
 
+# Build the markdown table for the links
 table = "\n\n| Name | Link |\n| --- | --- |\n"
-
 for name, link in links.items():
     table += f"| {name} | {link} |\n"
 table += "\n"
 
+# Update the table in readme
 with open('readme.md', 'r') as file :
   filedata = file.read()
 
